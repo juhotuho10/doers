7d889901331924e530f94068a6669e6fe5c6b5e3
Juho Naatula <juhotuho10@gmail.com>
2024-10-13 00:39:08 +0300

    added more default lints for easy linting

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+#![warn(
+    clippy::complexity,
+    clippy::correctness,
+    clippy::nursery,
+    clippy::perf,
+    clippy::style,
+    clippy::suspicious
+)]
 pub mod factorial_design;
 pub mod random_design;
 pub mod response_surface_design;
